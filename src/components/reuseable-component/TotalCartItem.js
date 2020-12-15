6b84aa74b3a9d3c43afb7ca5c7f678e710d7fae9
Binod Rajdhami <binodraazdhami@gmail.com>
2020-12-16 04:19:27 +0545

    update price and quantity in side cart item bar perfectly complete

--- src/components/reuseable-component/TotalCartItem.js
+++ src/components/reuseable-component/TotalCartItem.js
@@ -1,17 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import { CartItemContext } from './../../contextAPI/CartItemContext';
 
 const TotalCartItem = () => {
 
     const [cartItems] = useContext(CartItemContext);
+    const [itemQuantity, setItemQuantity] = useState(0);
+
+    useEffect(() => {
+        handleUpdateItems(cartItems);
+    });
+
+    const handleUpdateItems = (items) => {
+        let collectQuantity = items.map(e => e.quanty);
+        let total = 0;
+        for (let i in collectQuantity) {
+            total += collectQuantity[i];
+        }
+        setItemQuantity(total)
+    }
 
     return (
-        <>
-            {
-                cartItems.length > 0 ? cartItems.length : 0
-            }
-        </>
+        <>{itemQuantity}</>
     );
 }
 
--- src/components/reuseable-component/TotalPrice.js
+++ src/components/reuseable-component/TotalPrice.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from 'react';
+
+import { CartItemContext } from './../../contextAPI/CartItemContext';
 
 const TotalPrice = () => {
+
+    const [cartItems] = useContext(CartItemContext);
+    const [itemPrice, setItemPrice] = useState(0);
+
+    useEffect(() => {
+        handleUpdatePrice(cartItems);
+    });
+
+    const handleUpdatePrice = (items) => {
+        let collectPrice = items.map(e => e.price * e.quanty);
+        let total = 0;
+        for (let i in collectPrice) {
+            total += collectPrice[i];
+        }
+        setItemPrice(total)
+    }
+
     return (
         <>
-            <span>00.00</span>
+            {
+                itemPrice > 0 ? itemPrice : <span>00.00</span>
+            }
         </>
     );
 }
